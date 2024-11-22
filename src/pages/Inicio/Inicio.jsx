02d2ee3ace8a1f0228cba6a76ee3844058c0908b
DevsAndresa <andresaf150@gmail.com>
2024-11-22 14:27:58 -0300

    Adicionando o arquivo SCSS e removendo o arquivo CSS

--- src/pages/Inicio/Inicio.jsx
+++ src/pages/Inicio/Inicio.jsx
@@ -1,32 +1,26 @@
-import LivrosNasmaos from "../../assets/LivrosNasmaos.png";
 import community from "../../assets/community.png";
 import Livroaberto from "../../assets/Livroaberto.png";
 import transform from "../../assets/transform.png";
 import balance from "../../assets/balance.png";
 
-import "./Inicio.css";
+import s from "./inicio.module.scss";
 
 export default function Inicio() {
     return (
         <main>
-            <section className="inicio-section">
-                <img src={LivrosNasmaos} alt="Mãos segurando Livro" className="livro-imagem" />
-                <p className="texto-sobre-imagem">VENHA FAZER PARTE DA MAIOR REDE DE DOAÇÃO</p>
+            <section className={s.primeirasessaoinicio}>
+                <h2 className={s.textoSobreImagem}>VENHA FAZER PARTE DA MAIOR REDE DE DOAÇÃO</h2>
             </section>
-            <section className="pqdoar">
+            <section className={s.pqdoar}>
                 <h2>Por que devo doar?</h2>
-                <section className="retangulos">
-                <section className="retangulo1">
-                    <img
-                        src={community}
-                        alt="Mãos segurando Livro"
-                        className="retangulo-imagem1"
-                    />
-                    <p className="retangulo-texto1">
+                <section className={s.sessaoInformativos}>
+                 <section className={s.informativo}>
+                    <img src={community}  alt="Ilustração de mãos segurando Livro"/>
+                    <p className={s.textoretangulo1}>
                         Oferece livros a quem não tem acesso, ajudando a reduzir a exclusão social.
                     </p>
-                </section>
-                <section className="retangulo2">
+                 </section>
+                 <section className={s.informativo}>
                     <img
                         src={Livroaberto}
                         alt="Mãos segurando Livro"
@@ -34,8 +28,8 @@ export default function Inicio() {
                     />
                     <p className="retangulo-texto2">
                     Estimula o hábito da leitura e o aprendizado contínuo.                    </p>
-                </section>
-                <section className="retangulo3">
+                 </section>
+                 <section className={s.informativo}>
                     <img
                         src={transform}
                         alt="Mãos segurando Livro"
@@ -43,8 +37,8 @@ export default function Inicio() {
                     />
                     <p className="retangulo-texto3">
                     Fornece conhecimento e inspiração, permitindo que indivíduos transformem suas vidas.                   </p>
-                </section>
-                <section className="retangulo4">
+                 </section>
+                 <section className={s.informativo}>
                     <img
                         src={balance}
                         alt="Mãos segurando Livro"
