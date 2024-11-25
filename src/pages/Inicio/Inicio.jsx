c69755c8042335fd33d59154377eaed02bb218f5
DevsAndresa <andresaf150@gmail.com>
2024-11-25 10:40:32 -0300

    alterações inicio e footer

--- src/pages/Inicio/Inicio.jsx
+++ src/pages/Inicio/Inicio.jsx
@@ -19,23 +19,24 @@ export default function Inicio() {
                 <section className={s.sessaoInformativos}>
 
                  <section className={s.informativo}>
-                    <img src={community}  alt="Ilustração de mãos segurando Livro"/>
+                    <img src={community}  alt="Ilustração de figuras representando pessoas conectadas entre si representando inclusão"/>
                     <p> Oferece livros a quem não tem acesso, ajudando a reduzir a exclusão social.</p>
                  </section>
 
                  <section className={s.informativo}>
-                    <img src={Livroaberto} alt="Mãos segurando Livro"/>
+                    <img src={Livroaberto} alt="Ilustração de uma figura representando uma pessoa com o livro aberto demonstrando estar lendo"/>
                     <p>Estimula o hábito da leitura e o aprendizado contínuo.</p>
                  </section>
 
                  <section className={s.informativo}>
-                    <img src={transform} alt="Mãos segurando Livro"/>
+                    <img src={transform} alt="Ilustração de um punho cerrado dentro de um círculo, simbolizando união e força, conectado a uma fileira de figuras humanas representando pessoas"/>
                     <p> Fornece conhecimento e inspiração, permitindo que indivíduos transformem suas vidas. </p>
                  </section>
 
                  <section className={s.informativo}>
                     <img src={balance} alt="Ilustração de balança, símbolo da justiça."/>
-                    <p> Garante que todos, independentemente de sua condição, tenham oportunidades de aprendizado.</p>                </section>
+                    <p> Garante que todos, independentemente de sua condição, tenham oportunidades de aprendizado.</p>                
+                 </section>
                 </section>
 
             </section>
