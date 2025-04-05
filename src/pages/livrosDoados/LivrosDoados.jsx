67b1b66e33610a70367c8166a19f32fcead638fe
DevsAndresa <andresaf150@gmail.com>
2025-04-04 21:34:29 -0300

    Integração Api

--- src/pages/livrosDoados/LivrosDoados.jsx
+++ src/pages/livrosDoados/LivrosDoados.jsx
@@ -1,19 +1,37 @@
-import livroProtagonista from '../../assets/livroProtagonista.png'
 import s from './livrosDoados.module.scss'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
-export default function LivrosDoados(){
-    return(
+export default function LivrosDoados() {
+
+    const [livros, setLivros] = useState([])
+
+    const getLivros = async () => {
+        const resposta = await axios.get("https://livrosvainaweb-api-eiw8.onrender.com/livros")
+        setLivros(resposta.data)
+    }
+
+    useEffect(() => {
+        getLivros()
+        
+    }, [])
+
+    return (
         <section className={s.livrosDoadosSection}>
             <h2>Livros Doados</h2>
             <section className={s.containerCards}>
-                <section>
-                    <img src={livroProtagonista} alt="Imagem do livro o Protagonista" />
-                    <div>
-                        <h3>O protagonista</h3>
-                        <p>Susane Andrade</p>
-                        <p>Ficção</p>
-                    </div>
-                </section>
+                {livros.map((item) => (
+                    <section>
+                        <div>
+                            
+                            <img src={item.image_url} alt={item.titulo} />
+                            <h3>{item.titulo}</h3>
+                            <h4>{item.autor}</h4>
+                            <h4>{item.categoria}</h4>
+                        </div>
+                    </section>
+                ))}
+
             </section>
         </section>
     )
